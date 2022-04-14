d312f34005cc79ae9187fbd068693aa8e18b2ac0
jwc406 <77850103+jwc406@users.noreply.github.com>
2022-04-14 19:36:43 +0900

    리스트 join하여 하나의 문자열 생성

--- word_freq.py
+++ word_freq.py
@@ -2,8 +2,10 @@ import re
 
 f = open('textfile.txt', 'r', encoding='utf-8')
 
-text = f.readlines()
-text = list(map(lambda s: s.strip(), text))
+t = f.readlines()
+t = list(map(lambda s: s.strip(), t))
+
+text = " ".join(t)
 
 print(text)
 
