6eb815878de60630185f3fee723e97eff82aedc6
jwc406 <77850103+jwc406@users.noreply.github.com>
2022-04-14 20:36:42 +0900

    정렬하여 출력

--- word_freq.py
+++ word_freq.py
@@ -1,4 +1,6 @@
+from itertools import count
 import re
+import collections
 
 f = open('textfile.txt', 'r', encoding='utf-8')
 
@@ -9,6 +11,23 @@ text = " ".join(t)
 
 text = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', '', text)
 
-print(text)
+words = text.split()
+
+def get_counts(seq):
+    counts = {}
+    for i in seq:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+    return counts
+
+counts = get_counts(words)
+
+counts = sorted(counts.items(), key=lambda x:x[1], reverse=True)
+
+
+for key, value in counts[:5]:
+    print(format(key, "5"), str(value).rjust(5))
 
 f.close()
