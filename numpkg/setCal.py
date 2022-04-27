ce27d84bf5184f03804f12725c59df6283b80225
jwc406 <77850103+jwc406@users.noreply.github.com>
2022-04-27 17:11:13 +0900

    Update setCal.py

--- numpkg/setCal.py
+++ numpkg/setCal.py
@@ -1,5 +1,5 @@
 def setCal():
-	list_1st = list(map(int,input("input the 1st list: ").split()))
+    list_1st = list(map(int,input("input the 1st list: ").split()))
 	list_2nd = list(map(int,input("input the 2nd list: ").split()))
 
 	set_1st = set(list_1st)
